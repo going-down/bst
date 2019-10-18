b782637206d74ee90b574ca8235969d8ecb0e184
Oleksii Vovchok <a.vovchok.169@gmail.com>
2019-10-18 23:01:01 +0300

    module usage added

--- cmd/main.go
+++ cmd/main.go
@@ -4,19 +4,26 @@ import (
 	"github.com/going-down/bst/pkg/bst"
 )
 
+type tree bst.Tree
+
 func main() {
-	node := new(node)
-	tree := new(Tree)
-	val := new(ElementInt)
-	val.value = 100
-	node.value = val
-	tree.root = node
-	arr := []int{1, 16, 32, 2, 64, 128, 4, 8}
-	for _, v := range arr {
-		tree.Add(&ElementInt{value: v})
-	}
-	//tree.root.left = tree.root
-	//tree.root.right = tree.root
-	//	print(tree.root.value.(*ElementInt).value)
-	tree.Print()
+	t := tree{}
+
+	/*
+		node := new(node)
+		tree := new(Tree)
+		val := new(ElementInt)
+		val.value = 100
+		node.value = val
+		tree.root = node
+		arr := []int{1, 16, 32, 2, 64, 128, 4, 8}
+		for _, v := range arr {
+			tree.Add(&ElementInt{value: v})
+		}
+		//tree.root.left = tree.root
+		//tree.root.right = tree.root
+		//	print(tree.root.value.(*ElementInt).value)
+		tree.Print()
+
+	*/
 }
